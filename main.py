957a8968d98582cb4c7d0915b6a31a239182534a
woodsj1206 <47829885+woodsj1206@users.noreply.github.com>
2026-07-11 13:34:13 -0400

    refactor: use secrets module

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Date Created: 6/4/2026
 # Last Modified: 7/11/2026
 
-import random
+import secrets
 import argparse
 
 def parse_arguments():
@@ -42,19 +42,36 @@ def parse_arguments():
   return args
 
 
+def sample(sequence, n):
+  if n > len(sequence) or n < 0:
+    raise ValueError("Sample larger than length of sequence or is negative")
+  if n == 0:
+    return []
+
+  res = []
+  indices = [i for i in range(len(sequence))]
+
+  for i in range(n):
+    index = secrets.choice(indices)
+    res.append(sequence[index])
+    indices.remove(index)
+  
+  return res
+
+
 def generate_password(num_upper_chars, num_lower_chars, num_digits, num_special_chars):
   capital_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   lowercase_letters = capital_letters.lower()
   digits = "0123456789"
   specials = "~!@#$%^&*()_+-=`{}|:\"<>?[]\\;',./"
   
-  random_upper_chars = [random.choice(capital_letters) for i in range(num_upper_chars)]
-  random_lower_chars = [random.choice(lowercase_letters) for i in range(num_lower_chars)]
-  random_digits = [random.choice(digits) for i in range(num_digits)]
-  random_special_chars = [random.choice(specials) for i in range(num_special_chars)]
+  random_upper_chars = [secrets.choice(capital_letters) for i in range(num_upper_chars)]
+  random_lower_chars = [secrets.choice(lowercase_letters) for i in range(num_lower_chars)]
+  random_digits = [secrets.choice(digits) for i in range(num_digits)]
+  random_special_chars = [secrets.choice(specials) for i in range(num_special_chars)]
   
   password = random_upper_chars + random_lower_chars + random_digits + random_special_chars
-  shuffled_password = random.sample(password, len(password))
+  shuffled_password = sample(password, len(password))
 
   return "".join(shuffled_password)
 
